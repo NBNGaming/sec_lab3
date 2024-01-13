8b0964e16bcf7840b5c616514e299c477f899785
Николай Батычко <nbngamings@gmail.com>
2024-01-13 05:52:25 +0300

    Initial commit (lab 2)

---
+++ app/src/main/java/com/example/inventory/ui/item/ItemEntryViewModel.kt
@@ -0,0 +1,127 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.inventory.ui.item
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.inventory.data.Item
+import com.example.inventory.data.ItemsRepository
+import java.text.NumberFormat
+
+/**
+ * ViewModel to validate and insert items in the Room database.
+ */
+class ItemEntryViewModel(private val itemsRepository: ItemsRepository) : ViewModel() {
+
+    /**
+     * Holds current item ui state
+     */
+    var itemUiState by mutableStateOf(ItemUiState())
+        private set
+
+    /**
+     * Updates the [itemUiState] with the value provided in the argument. This method also triggers
+     * a validation for input values.
+     */
+    fun updateUiState(itemDetails: ItemDetails) {
+        itemUiState =
+            ItemUiState(itemDetails = itemDetails, isEntryValid = validateInput(itemDetails))
+    }
+
+    /**
+     * Inserts an [Item] in the Room database
+     */
+    suspend fun saveItem() {
+        if (validateInput()) {
+            itemsRepository.insertItem(itemUiState.itemDetails.toItem())
+        }
+    }
+
+    private fun validateInput(uiState: ItemDetails = itemUiState.itemDetails): Boolean {
+        return with(uiState) {
+            name.isNotBlank() && price.isNotBlank() && quantity.isNotBlank() && email.isNotBlank() && number.isNotBlank() && supplier.isNotBlank()
+        }
+    }
+}
+
+/**
+ * Represents Ui State for an Item.
+ */
+data class ItemUiState(
+    val itemDetails: ItemDetails = ItemDetails(),
+    val isEntryValid: Boolean = false
+)
+
+data class ItemDetails(
+    val id: Int = 0,
+    val name: String = "",
+    val price: String = "",
+    val quantity: String = "",
+    val email: String = "",
+    val number: String = "",
+    val supplier: String = "",
+)
+
+/**
+ * Extension function to convert [ItemUiState] to [Item]. If the value of [ItemDetails.price] is
+ * not a valid [Double], then the price will be set to 0.0. Similarly if the value of
+ * [ItemUiState] is not a valid [Int], then the quantity will be set to 0
+ */
+fun ItemDetails.toItem(): Item {
+    val emailRegex = Regex("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}")
+    val phoneNumberRegex = Regex("^8\\d{10}$")
+
+    val isValidEmail = if (emailRegex.matches(email)) email else ""
+    val isValidNumber = if (phoneNumberRegex.matches(number)) number else ""
+
+    return Item(
+        id = id,
+        name = name,
+        price = price.toDoubleOrNull() ?: 0.0,
+        quantity = quantity.toIntOrNull() ?: 0,
+        email = isValidEmail,
+        number = isValidNumber,
+        supplier = supplier
+    )
+}
+
+fun Item.formatedPrice(): String {
+    return NumberFormat.getCurrencyInstance().format(price)
+}
+
+/**
+ * Extension function to convert [Item] to [ItemUiState]
+ */
+fun Item.toItemUiState(isEntryValid: Boolean = false): ItemUiState = ItemUiState(
+    itemDetails = this.toItemDetails(),
+    isEntryValid = isEntryValid
+)
+
+/**
+ * Extension function to convert [Item] to [ItemDetails]
+ */
+fun Item.toItemDetails(): ItemDetails = ItemDetails(
+    id = id,
+    name = name,
+    price = price.toString(),
+    quantity = quantity.toString(),
+    supplier = supplier,
+    email = email,
+    number = number
+)
